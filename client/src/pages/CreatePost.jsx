6b624b02ff68d438803730c0718b3699f9d02ae8
DavGonzaj <davgonzaj@gmail.com>
2023-05-27 22:49:32 -0700

    From Field started

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -14,6 +14,16 @@ const CreatePost = () => {
   const [generatingImg, setGeneratingImg] = useState(false)
   const [loading,setLoading] = useState(false)
 
+  const handleSubmit = ()=>{
+
+  }
+
+  const handleChange = (e)=>{
+
+  }
+
+  const handleSurpriseMe = ()=>{}
+
     return (
     <section className='max-w-7xl mx-auto'>
         <div>
@@ -21,8 +31,27 @@ const CreatePost = () => {
             <p className='mt-2 text-[#666e75] text-[16px] max-w-[500px]'> Create imaginative and visually stunning images through DALL-E AI and share them with the community  </p>
         </div>
 
-<form className=''>
-
+<form className='mt-16 max-w-3xl' onSubmit={handleSubmit}>
+      <div className='fle flex-col gap-5'>
+          <FormField
+            labelName="Your name"
+            type="text"
+            name="name"
+            placeholder="Your name here"
+            value={form.name}
+            handleChange={handleChange}
+          />
+          <FormField
+            labelName="Prompt"
+            type="text"
+            name="prompt"
+            placeholder="an armchair in the shape of an avocado"
+            value={form.prompt}
+            handleChange={handleChange}
+            isSurpriseMe
+            handleSurpriseMe={handleSurpriseMe}
+          />
+      </div>
 </form>
 
     </section>
